6733c1f591dfe574d53adf86ab8430ae24cd2d1d
Avery Koranda <koran023@morris.umn.edu>
2019-03-06 22:00:08 -0600

    Write getRides to simply return all existing rides
    Also copied the helper method `serializeIterable()` over from UserController to assist getRides().

--- server/src/main/java/umm3601/ride/RideController.java
+++ server/src/main/java/umm3601/ride/RideController.java
@@ -1,4 +1,49 @@
 package umm3601.ride;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public class RideController {
+
+  private final MongoCollection<Document> rideCollection;
+
+  /**
+   * Construct a controller for rides.
+   *
+   * @param database the database containing ride data
+   */
+  public RideController(MongoDatabase database) {
+    rideCollection = database.getCollection("rides");
+  }
+
+  /**
+   * Helper method which returns all existing rides.
+   *
+   * @return an array of Rides in a JSON formatted string
+   */
+  public String getRides() {
+
+    // server-side filtering will happen here if we sell that in future stories.
+    // Right now, this method simply returns all existing rides.
+
+    FindIterable<Document> matchingRides = rideCollection.find();
+
+    return serializeIterable(matchingRides);
+  }
+
+  /**
+   * Takes an iterable collection of documents, turn each into JSON string
+   * using `document.toJson`, and then join those strings into a single
+   * string representing an array of JSON objects.
+   */
+  private String serializeIterable(Iterable<Document> documents) {
+    return StreamSupport.stream(documents.spliterator(), false)
+      .map(Document::toJson)
+      .collect(Collectors.joining(", ", "[", "]"));
+  }
 }
