87a51dfecbcd0b225ebb50d1ce4fa1780db976b8
Avery Koranda <koran023@morris.umn.edu>
2019-03-07 14:51:29 -0600

    Comment out annoying user test that always shows up as pending when run

--- client/src/app/users/add-user.component.spec.ts
+++ client/src/app/users/add-user.component.spec.ts
@@ -42,30 +42,30 @@ describe('Add user component', () => {
   // https://stackoverflow.com/questions/52046741/angular-testbed-query-by-css-find-the-pseudo-element
   // https://angular.io/guide/form-validation
   // https://github.com/angular/angular/blob/7.2.2/packages/forms/src/validators.ts#L136-L157
-  it('should not allow a name to contain a symbol'), async(() => {
-    let fixture = TestBed.createComponent(AddUserComponent);
-    let debug = fixture.debugElement;
-    let input = debug.query(By.css('[name=email]'));
-
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      input.nativeElement.value = 'bad@email.com';
-      dispatchEvent(input.nativeElement);
-      fixture.detectChanges();
-
-      let form: NgForm = debug.children[0].injector.get(NgForm);
-      let control = form.control.get('email');
-      expect(control.hasError('notPeeskillet')).toBe(true);
-      expect(form.control.valid).toEqual(false);
-      expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(true);
-
-      input.nativeElement.value = 'peeskillet@stackoverflow.com';
-      dispatchEvent(input.nativeElement);
-      fixture.detectChanges();
-
-      expect(control.hasError('notPeeskillet')).toBe(false);
-      expect(form.control.valid).toEqual(true);
-      expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(false);
-    });
-  });
+  // it('should not allow a name to contain a symbol'), async(() => {
+  //   let fixture = TestBed.createComponent(AddUserComponent);
+  //   let debug = fixture.debugElement;
+  //   let input = debug.query(By.css('[name=email]'));
+  //
+  //   fixture.detectChanges();
+  //   fixture.whenStable().then(() => {
+  //     input.nativeElement.value = 'bad@email.com';
+  //     dispatchEvent(input.nativeElement);
+  //     fixture.detectChanges();
+  //
+  //     let form: NgForm = debug.children[0].injector.get(NgForm);
+  //     let control = form.control.get('email');
+  //     expect(control.hasError('notPeeskillet')).toBe(true);
+  //     expect(form.control.valid).toEqual(false);
+  //     expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(true);
+  //
+  //     input.nativeElement.value = 'peeskillet@stackoverflow.com';
+  //     dispatchEvent(input.nativeElement);
+  //     fixture.detectChanges();
+  //
+  //     expect(control.hasError('notPeeskillet')).toBe(false);
+  //     expect(form.control.valid).toEqual(true);
+  //     expect(form.control.hasError('notPeeskillet', ['email'])).toEqual(false);
+  //   });
+  // });
 });
