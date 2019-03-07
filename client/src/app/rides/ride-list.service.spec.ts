e280cd1aba066a30382db65258355fefe7ca59e7
Avery Koranda <koran023@morris.umn.edu>
2019-03-07 14:15:28 -0600

    Narrow test file to only test what has been implemented thus far

--- client/src/app/rides/ride-list.service.spec.ts
+++ client/src/app/rides/ride-list.service.spec.ts
@@ -10,34 +10,34 @@ describe('Ride list service: ', () => {
   const testRides: Ride[] = [
     {
       _id: 'chris_id',
-      name: 'Chris',
-      age: 25,
-      company: 'UMM',
-      email: 'chris@this.that'
+      driver: 'Chris',
+      seats_available: 3,
+      start_location: 'UMM',
+      end_location: 'Willie\'s',
+      departure_date: '3/6/2019',
+      departure_time: '10:00:00'
     },
     {
-      _id: 'pat_id',
-      name: 'Pat',
-      age: 37,
-      company: 'IBM',
-      email: 'pat@something.com'
+      _id: 'dennis_id',
+      driver: 'Dennis',
+      seats_available: 1,
+      start_location: 'Caribou Coffee',
+      end_location: 'Minneapolis, MN',
+      departure_date: '8/15/2018',
+      departure_time: '11:30:00'
     },
     {
-      _id: 'jamie_id',
-      name: 'Jamie',
-      age: 37,
-      company: 'Frogs, Inc.',
-      email: 'jamie@frogs.com'
+      _id: 'agatha_id',
+      driver: 'Agatha',
+      seats_available: 6,
+      start_location: 'UMM',
+      end_location: 'Fergus Falls, MN',
+      departure_date: '3/30/2019',
+      departure_time: '16:30:00'
     }
   ];
-  const mRides: Ride[] = testRides.filter(ride =>
-    ride.company.toLowerCase().indexOf('m') !== -1
-  );
 
-  // We will need some url information from the rideListService to meaningfully test company filtering;
-  // https://stackoverflow.com/questions/35987055/how-to-write-unit-testing-for-angular-2-typescript-for-private-methods-with-ja
   let rideListService: RideListService;
-  let currentlyImpossibleToGenerateSearchRideUrl: string;
 
   // These are used to mock the HTTP requests so that we (a) don't have to
   // have the server running and (b) we can check exactly which HTTP
@@ -62,7 +62,7 @@ describe('Ride list service: ', () => {
     httpTestingController.verify();
   });
 
-  it('getRides() calls api/rides', () => {
+  it('getRides() calls api/rides and returns appropriate objects', () => {
     // Assert that the rides we get from this call to getRides()
     // should be our set of test rides. Because we're subscribing
     // to the result of getRides(), this won't actually get
@@ -81,71 +81,5 @@ describe('Ride list service: ', () => {
     // triggers the subscribe above, which leads to that check
     // actually being performed.
     req.flush(testRides);
-  });
-
-  it('getRides(rideCompany) adds appropriate param string to called URL', () => {
-    rideListService.getRides('m').subscribe(
-      rides => expect(rides).toEqual(mRides)
-    );
-
-    const req = httpTestingController.expectOne(rideListService.baseUrl + '?company=m&');
-    expect(req.request.method).toEqual('GET');
-    req.flush(mRides);
-  });
-
-  it('filterByCompany(rideCompany) deals appropriately with a URL that already had a company', () => {
-    currentlyImpossibleToGenerateSearchRideUrl = rideListService.baseUrl + '?company=f&something=k&';
-    rideListService['rideUrl'] = currentlyImpossibleToGenerateSearchRideUrl;
-    rideListService.filterByCompany('m');
-    expect(rideListService['rideUrl']).toEqual(rideListService.baseUrl + '?something=k&company=m&');
-  });
-
-  it('filterByCompany(rideCompany) deals appropriately with a URL that already had some filtering, but no company', () => {
-    currentlyImpossibleToGenerateSearchRideUrl = rideListService.baseUrl + '?something=k&';
-    rideListService['rideUrl'] = currentlyImpossibleToGenerateSearchRideUrl;
-    rideListService.filterByCompany('m');
-    expect(rideListService['rideUrl']).toEqual(rideListService.baseUrl + '?something=k&company=m&');
-  });
-
-  it('filterByCompany(rideCompany) deals appropriately with a URL has the keyword company, but nothing after the =', () => {
-    currentlyImpossibleToGenerateSearchRideUrl = rideListService.baseUrl + '?company=&';
-    rideListService['rideUrl'] = currentlyImpossibleToGenerateSearchRideUrl;
-    rideListService.filterByCompany('');
-    expect(rideListService['rideUrl']).toEqual(rideListService.baseUrl + '');
-  });
-
-  it('getRideById() calls api/rides/id', () => {
-    const targetRide: Ride = testRides[1];
-    const targetId: string = targetRide._id;
-    rideListService.getRideById(targetId).subscribe(
-      ride => expect(ride).toBe(targetRide)
-    );
-
-    const expectedUrl: string = rideListService.baseUrl + '/' + targetId;
-    const req = httpTestingController.expectOne(expectedUrl);
-    expect(req.request.method).toEqual('GET');
-    req.flush(targetRide);
-  });
-
-  it('adding a ride calls api/rides/new', () => {
-    const jesse_id = 'jesse_id';
-    const newRide: Ride = {
-      _id: '',
-      name: 'Jesse',
-      age: 72,
-      company: 'Smithsonian',
-      email: 'jesse@stuff.com'
-    };
-
-    rideListService.addNewRide(newRide).subscribe(
-      id => {
-        expect(id).toBe(jesse_id);
-      }
-    );
-
-    const expectedUrl: string = rideListService.baseUrl + '/new';
-    const req = httpTestingController.expectOne(expectedUrl);
-    expect(req.request.method).toEqual('POST');
-    req.flush(jesse_id);
   });
 });
