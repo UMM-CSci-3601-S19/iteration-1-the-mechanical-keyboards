a38cb9574247376191ab4765145a1a2aab8e53a6
michael-small <small203@morris.umn.edu>
2019-03-09 14:25:29 -0600

    Start AddRide backend with addNewRide function

--- server/src/main/java/umm3601/ride/RideRequestHandler.java
+++ server/src/main/java/umm3601/ride/RideRequestHandler.java
@@ -24,4 +24,23 @@ public class RideRequestHandler {
     res.type("application/json");
     return rideController.getRides();
   }
+
+  public String addNewRide(Request req, Response res) {
+    res.type("application/json");
+
+    Document newRide = Document.parse(req.body());
+
+    String driver = newRide.getString("driver");
+    String notes = newRide.getString("notes");
+    int seatsAvailable = newRide.getInteger("seatsAvailable");
+    String origin = newRide.getString("origin");
+    String destination = newRide.getString("destination");
+    String departureTime = newRide.getString("departureTime");
+    String departureDate = newRide.getString("departureDate");
+
+    System.err.println("Adding new ride [driver=" + driver + ", notes=" + notes + " seatsAvailable=" + seatsAvailable
+      + " origin=" + origin + " destination=" + destination + " departureTime=" + departureTime + " departureDate="
+      + departureDate + ']');
+    return rideController.addNewRide(driver, notes, seatsAvailable, origin, destination, departureTime, departureDate);
+  }
 }
