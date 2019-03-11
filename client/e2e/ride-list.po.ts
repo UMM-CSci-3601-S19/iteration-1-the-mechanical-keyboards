b08c274e0cc70048d793c154c8e94ee808258da4
Avery Koranda <koran023@morris.umn.edu>
2019-03-10 20:56:25 -0500

    Create first e2e ride test and set up basic functions in the page object

--- client/e2e/ride-list.po.ts
+++ client/e2e/ride-list.po.ts
@@ -3,96 +3,34 @@ import {Key} from 'selenium-webdriver';
 
 export class RidePage {
   navigateTo(): promise.Promise<any> {
-    return browser.get('/users');
-  }
-
-  // http://www.assertselenium.com/protractor/highlight-elements-during-your-protractor-test-run/
-  highlightElement(byObject) {
-    function setStyle(element, style) {
-      const previous = element.getAttribute('style');
-      element.setAttribute('style', style);
-      setTimeout(() => {
-        element.setAttribute('style', previous);
-      }, 200);
-      return 'highlighted';
-    }
-
-    return browser.executeScript(setStyle, element(byObject).getWebElement(), 'color: red; background-color: yellow;');
+    return browser.get('/rides');
   }
 
   getRideTitle() {
-    const title = element(by.id('ride-list-title')).getText();
-    this.highlightElement(by.id('ride-list-title'));
-
-    return title;
+    return element(by.id('ride-list-title')).getText();
   }
 
-  typeADriverName(name: string) {
-    const input = element(by.id('driverName'));
-    input.click();
-    input.sendKeys(name);
+  getRides() {
+    return element.all(by.className('rides'));
   }
 
-  selectUpKey() {
-    browser.actions().sendKeys(Key.ARROW_UP).perform();
+  elementExistsWithId(id: string): promise.Promise<boolean> {
+    return element(by.id(id)).isPresent();
   }
 
-  backspace() {
-    browser.actions().sendKeys(Key.BACK_SPACE).perform();
+  elementExistsWithCss(css: string): promise.Promise<boolean> {
+    return element(by.css(css)).isPresent();
   }
 
-  getSeatNumber(company: string) {
-    const input = element(by.id('userSeatsAvailable'));
-    input.click();
-    input.sendKeys(company);
-    this.click('submit');
+  getElementById(id: string) {
+    return element(by.id(id));
   }
 
-  getUserByAge() {
-    const input = element(by.id('userName'));
-    input.click();
-    input.sendKeys(Key.TAB);
-  }
-
-  getUniqueUser(email: string) {
-    const user = element(by.id(email)).getText();
-    this.highlightElement(by.id(email));
-
-    return user;
-  }
-
-  getUsers() {
-    return element.all(by.className('users'));
-  }
-
-  elementExistsWithId(idOfElement: string): promise.Promise<boolean> {
-    if (element(by.id(idOfElement)).isPresent()) {
-      this.highlightElement(by.id(idOfElement));
-    }
-    return element(by.id(idOfElement)).isPresent();
-  }
-
-  elementExistsWithCss(cssOfElement: string): promise.Promise<boolean> {
-    return element(by.css(cssOfElement)).isPresent();
+  getElementsByCss(css: string) {
+    return element.all(by.css(css));
   }
 
   click(idOfButton: string): promise.Promise<void> {
-    this.highlightElement(by.id(idOfButton));
     return element(by.id(idOfButton)).click();
   }
-
-  field(idOfField: string) {
-    return element(by.id(idOfField));
-  }
-
-  button(idOfButton: string) {
-    this.highlightElement(by.id(idOfButton));
-    return element(by.id(idOfButton));
-  }
-
-  getTextFromField(idOfField: string) {
-    this.highlightElement(by.id(idOfField));
-    return element(by.id(idOfField)).getText();
-  }
-
 }
